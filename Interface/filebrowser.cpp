3136488f7fc81a99823faa2b80efd1fb4dbd5f20
saha97-pal <54217209+saha97-pal@users.noreply.github.com>
2019-11-14 11:33:11 -0500

    FileBrowser update

--- Interface/filebrowser.cpp
+++ Interface/filebrowser.cpp
@@ -9,14 +9,14 @@ FileBrowser::FileBrowser()
 QString FileBrowser::getOpenFileName(MainWindow *w)
 {
     //QString resourceDir = QCoreApplication::applicationDirPath() + "/Resources";
-    QString resourceDir = QCoreApplication::applicationDirPath().section("/", 0, -2) + "/SCAPES/Resources";
+    QString resourceDir = QCoreApplication::applicationDirPath().section("/", 0, -3) + "/SCAPES/Resources";
     QString fileName = QFileDialog::getOpenFileName(w, "Open file", resourceDir);
 
     return fileName;
 }
 QString FileBrowser::getSaveFileName(MainWindow *w)
 {
-    QString resourceDir = QCoreApplication::applicationDirPath().section("/", 0, -2) + "/SCAPES/Resources";
+    QString resourceDir = QCoreApplication::applicationDirPath().section("/", 0, -3) + "/SCAPES/Resources";
     QString fileName = QFileDialog::getSaveFileName(w, "Save As", resourceDir);
 
     return fileName;
